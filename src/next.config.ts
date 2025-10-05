dae4f03cfa46510958ede2ff50f38ed4cf6fe026
Nious park <niouspark@gmail.com>
2025-10-05 15:27:45 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -33,7 +34,7 @@ const nextConfig: NextConfig = {
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/**',
-      }
+      },
     ],
   },
 };
