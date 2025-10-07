a4909fe994ca24f81bbc5351af82ddc73c3a6eff
Nious park <niouspark@gmail.com>
2025-10-07 18:14:02 +0000

    meaning?? 8:07:11.978  18:07:10.490 Ably: Auth.requestToken(): token req

--- src/lib/ably.ts
+++ src/lib/ably.ts
@@ -2,4 +2,4 @@ import { Realtime } from 'ably';
 
 // The client doesn't use the API key directly.
 // It will use a URL to fetch a token from our own server.
-export const ably = new Realtime({ authUrl: '/api/ably-token' });
+export const ably = new Realtime({ authUrl: 'api/ably-token' });
