71ab252df3cdb37446eeef725c501dba06fc5dad
Nious park <niouspark@gmail.com>
2025-10-07 18:11:11 +0000

    main.realtime.ably.net/keys/M2Zk9w.SO4h8A/requestToken?rnd=2838248643638

--- src/app/api/ably-token/route.ts
+++ src/app/api/ably-token/route.ts
@@ -8,8 +8,7 @@ export async function GET(req: NextRequest) {
   if (!ablyApiKey) {
     return NextResponse.json(
       {
-        errorMessage: `Missing ABLY_API_KEY environment variable.
-        If you're running locally, please ensure you have a .env file with a value for ABLY_API_KEY`,
+        errorMessage: `Missing ABLY_API_KEY environment variable.`,
       },
       {
         status: 500,
@@ -18,8 +17,8 @@ export async function GET(req: NextRequest) {
     );
   }
 
-  // We're using the REST client here to create a token request.
-  // The client on the frontend will then use this token to authenticate.
+  // IMPORTANT: Instantiate the client *inside* the handler for serverless environments.
+  // This ensures the API key is read from the environment variables on every request.
   const client = new Ably.Rest(ablyApiKey);
   const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'hostel-hq-student' });
 
