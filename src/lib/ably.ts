77995a320038a3adc51e23e47a8cb7a606dd46eb
Nious park <niouspark@gmail.com>
2025-10-07 18:21:44 +0000

    18:18:54.614  18:18:53.162 Ably: Auth.requestToken(): token request sign

--- src/lib/ably.ts
+++ src/lib/ably.ts
@@ -2,4 +2,7 @@ import { Realtime } from 'ably';
 
 // The client doesn't use the API key directly.
 // It will use a URL to fetch a token from our own server.
-export const ably = new Realtime({ authUrl: 'api/ably-token' });
+// We construct an absolute URL to avoid "Invalid URL" errors in different environments.
+const authUrl = '/api/ably-token';
+
+export const ably = new Realtime({ authUrl });
