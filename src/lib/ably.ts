69b8d0559eba25e053112df1a1cf068da229eb80
Nious park <niouspark@gmail.com>
2025-10-10 15:49:06 +0000

    what is happening f6d0a8ec-a122345e59f3218e.js:1 15:46:19.228 Ably: Auth

--- src/lib/ably.ts
+++ src/lib/ably.ts
@@ -4,7 +4,7 @@ import { Realtime } from 'ably';
 // The client doesn't use the API key directly.
 // It will use a URL to fetch a token from our own server.
 // We use a root-relative path to ensure it always points to the correct endpoint.
-const authUrl = 'api/ably-token';
+const authUrl = '/api/ably-token';
 
 let ablyInstance: Realtime | null = null;
 
